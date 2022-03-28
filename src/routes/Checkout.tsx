fb52abf5024a79d226f9227780ee582b0b92ccd6
tingmon <Tjeong2320@conestogac.on.ca>
2022-03-28 03:52:37 -0400

    checkout ongoing

--- src/routes/Checkout.tsx
+++ src/routes/Checkout.tsx
@@ -10,13 +10,15 @@ import {
 	paymentInfoAtom,
 	priceTotalInfoAtom,
 	totalInfoAtom,
+	uidAtom,
 	userObjectAtom,
 } from "../atoms";
 import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { dbService, firebaseInstance } from "../fbase";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Collapse, FormControlLabel, Switch } from "@mui/material";
+import Swal from "sweetalert2";
 
 const Container = styled.div`
 	max-width: 480px;
@@ -106,12 +108,31 @@ const SubmitBtn = styled.button`
 	font-weight: bold;
 `;
 
-function Checkout() {
-	const [isLoading, setIsLoading] = useState(true);
-	const [userInfo, setUserInfo] = useState<any>(null);
-	const [isShipping, setIsShipping] = useState(true);
-	const [isBilling, setIsBilling] = useState(false);
+const DisabledBtn = styled.button`
+	text-align: center;
+	background: #04aaff;
+	color: white;
+	margin-top: 10px;
+	cursor: not-allowed;
 
+	max-width: 320px;
+	width: 300px;
+	padding: 10px;
+	border-radius: 30px;
+	background-color: rgba(255, 255, 255, 1);
+	margin-bottom: 10px;
+	font-size: 12px;
+	color: black;
+	font-weight: bold;
+`;
+
+function Checkout() {
+	const history = useHistory();
+	const [isLoading, setIsLoading] = useState(true);
+	const [userInfo, setUserInfo] = useState<any>([]);
+	const [isReady, setIsReady] = useState(false);
+
+	const uid = useRecoilValue(uidAtom);
 	const userObject = useRecoilValue(userObjectAtom);
 	const totalInfo = useRecoilValue(priceTotalInfoAtom);
 	const [paymentInfo, setPaymentInfo] = useRecoilState(paymentInfoAtom);
@@ -126,7 +147,8 @@ function Checkout() {
 					id: doc.id,
 					...doc.data(),
 				}));
-				setUserInfo(recordSnapshot[0]);
+				console.log(recordSnapshot);
+				setUserInfo(recordSnapshot);
 			});
 	}
 
@@ -139,7 +161,12 @@ function Checkout() {
 					id: doc.id,
 					...doc.data(),
 				}));
-				setPaymentInfo(paymentSnapshot[0]);
+				if (paymentSnapshot[0].vendor == "") {
+					setIsReady(false);
+				} else {
+					setIsReady(true);
+				}
+				setPaymentInfo(paymentSnapshot);
 			});
 	}
 
@@ -152,68 +179,47 @@ function Checkout() {
 					id: doc.id,
 					...doc.data(),
 				}));
-				setAddressInfo(addressSnapshot[0]);
+				if (
+					addressSnapshot[0].shippingAddress.postalcode == "" ||
+					addressSnapshot[0].billingAddress.postalcode == ""
+				) {
+					setIsReady(false);
+				} else {
+					setIsReady(true);
+				}
+				setAddressInfo(addressSnapshot);
 			});
 	}
 
-	const createPaymentInfo = () => {
-		const paymentInfoValue = {
-			uid: userObject.uid,
-			cardNumber: "",
-			expiryMonth: "",
-			expiryYear: "",
-			cvv: "",
-			vendor: "",
-		};
-		await dbService.collection("PaymentInfo").add(paymentInfoValue);
-		console.log("new payment info");
-	};
-
-	const createAddressInfo = () => {
-		const addressInfoValue = {
-			uid: userObject.uid,
-			shippingAddress: {
-				firstName: "",
-				lastName: "",
-				phoneNumber: "",
-				address1: "",
-				address2: "",
-				city: "",
-				province: "",
-				postalcode: "",
-			},
-			billingAddress: {
-				firstName: "",
-				lastName: "",
-				phoneNumber: "",
-				address1: "",
-				address2: "",
-				city: "",
-				province: "",
-				postalcode: "",
-			},
-		};
-		await dbService.collection("AddressInfo").add(addressInfoValue);
-		console.log("new address info");
-	};
-
-	const handleIsShipping = () => {
-		setIsShipping((prev) => !prev);
-	};
-	const handleIsBilling = () => {
-		setIsBilling((prev) => !prev);
-	};
-
 	//
 	useEffect(() => {
-		fetchUserInfo(userObject.uid);
-		fetchPaymentInfo(userObject.uid);
-		fetchAddressInfo(userObject.uid);
+		fetchPaymentInfo(uid);
+		fetchAddressInfo(uid);
+		fetchUserInfo(uid);
 		setIsLoading(false);
 	}, []);
 
 	// make new transaction record / userinfo -> update buyer point, seller point, rank
-	const onSubmitClick = () => {};
+	const onSubmitClick = async () => {
+		Swal.fire({
+			title: "Your Order is Placed!",
+			// showDenyButton: true,
+			confirmButtonText: "Got It",
+		}).then((result) => {
+			/* Read more about isConfirmed, isDenied below */
+			if (result.isConfirmed) {
+				// Swal.fire("Saved!", "", "success");
+
+				const transactionRecord = {};
+
+				history.push(`/`);
+			}
+			//  else if (result.isDenied) {
+			// 	Swal.fire("Changes are not saved", "", "info");
+			// 	// history.push("/cart");
+			// }
+		});
+	};
 
 	console.log("userInfo: ", userInfo);
 	console.log("paymentInfo: ", paymentInfo);
@@ -225,57 +231,90 @@ function Checkout() {
 				"Please Wait..."
 			) : (
 				<>
-					{/* {paymentInfo == null && createPaymentInfo() : <></>} */}
-					<Container>
-						<FormControlLabel
-							control={
-								<Switch checked={isShipping} onChange={handleIsShipping} />
-							}
-							label="SHIPPING ADDRESS"
-						/>
-						<Collapse></Collapse>
-						<Shipto>
-							<HeaderText>SHIP TO</HeaderText>
-							<Text>recipient</Text>
-							<Text>address1(street)</Text>
-							<Text>address2(addtional)</Text>
-							<Text>city</Text>
-							<Text>postalcode</Text>
-						</Shipto>
-						<PaymentDetails>
-							<HeaderText>CREDIT CARD DETAILS</HeaderText>
-							<Text>Visa **** 7209</Text>
-						</PaymentDetails>
-						<BillingAddress>
-							<HeaderText>BILLING ADDRESS</HeaderText>
-							<Text>recipient</Text>
-							<Text>address1(street)</Text>
-							<Text>address2(addtional)</Text>
-							<Text>city</Text>
-							<Text>postalcode</Text>
-						</BillingAddress>
-						<Total>
-							<Label>
-								<Text>Subtotal: </Text>
-								<Text>${totalInfo.subtotal}</Text>
-							</Label>
-							<Label>
-								<Text>Shipping: </Text>
-								<Text>${totalInfo.shipping}</Text>
-							</Label>
-							<Label>
-								<TotalText>Total: </TotalText>
-								<TotalText>${totalInfo.total}</TotalText>
-							</Label>
-						</Total>
-						<Link
-							to={{
-								pathname: `/`,
-							}}
-						>
-							<SubmitBtn>PLACE ORDER</SubmitBtn>
-						</Link>
-					</Container>
+					{addressInfo[0] && paymentInfo[0] && (
+						<Container>
+							{/* <div>
+								{addressInfo[0]?.billingAddress.postalcode == "" &&
+									addressInfo[0]?.shippingAddress.postalcode == "" && (
+										<Link
+											to={`/${uid}/profile/`}
+											params={{ fromCheckout: true }}
+										>
+											<button>Go to Address Form</button>
+										</Link>
+									)}
+								{paymentInfo[0]?.vendor == "" && (
+									<Link to={`/${uid}/profile/`} params={{ fromCheckout: true }}>
+										<button>Go to Payment Form</button>
+									</Link>
+								)}
+							</div> */}
+
+							<Shipto>
+								<HeaderText>SHIP TO</HeaderText>
+								<Text>
+									{addressInfo[0]?.shippingAddress.firstName}{" "}
+									{addressInfo[0]?.shippingAddress.lastName}
+								</Text>
+								<Text>{addressInfo[0]?.shippingAddress.address1}</Text>
+								<Text>{addressInfo[0]?.shippingAddress.address2}</Text>
+								<Text>{addressInfo[0]?.shippingAddress.city}</Text>
+								<Text>{addressInfo[0]?.shippingAddress.postalcode}</Text>
+							</Shipto>
+							<PaymentDetails>
+								<HeaderText>CREDIT CARD</HeaderText>
+								<Text>
+									{paymentInfo[0]?.vendor} {" **** "}{" "}
+									{paymentInfo[0]?.cardNumber.slice(12)}
+								</Text>
+							</PaymentDetails>
+							<BillingAddress>
+								<HeaderText>BILLING ADDRESS</HeaderText>
+								<Text>
+									{addressInfo[0]?.billingAddress.firstName}{" "}
+									{addressInfo[0]?.billingAddress.lastName}
+								</Text>
+								<Text>{addressInfo[0]?.billingAddress.address1}</Text>
+								<Text>{addressInfo[0]?.billingAddress.address2}</Text>
+								<Text>{addressInfo[0]?.billingAddress.city}</Text>
+								<Text>{addressInfo[0]?.billingAddress.postalcode}</Text>
+							</BillingAddress>
+							<Total>
+								<Label>
+									<Text>Subtotal: </Text>
+									<Text>${totalInfo.subtotal}</Text>
+								</Label>
+								<Label>
+									<Text>Shipping: </Text>
+									<Text>${totalInfo.shipping}</Text>
+								</Label>
+								<Label>
+									<TotalText>Total: </TotalText>
+									<TotalText>${totalInfo.total}</TotalText>
+								</Label>
+							</Total>
+							<a>
+								{!isReady ? (
+									<DisabledBtn disabled>COMPLETE INFO FIRST</DisabledBtn>
+								) : (
+									<SubmitBtn
+										onClick={() => {
+											onSubmitClick();
+										}}
+									>
+										PLACE ORDER
+									</SubmitBtn>
+								)}
+							</a>
+							<Link
+								to={{
+									pathname: `/cart`,
+								}}
+							>
+								<SubmitBtn>BACK</SubmitBtn>
+							</Link>
+						</Container>
+					)}
 				</>
 			)}
 		</>
